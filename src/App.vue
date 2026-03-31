a26efa4b3dd905e3385fb6f310ead1300ce26ee2
israelonilink <israel_cursos2011@hotmail.com>
2026-03-30 21:20:07 -0600

    42. Single File Components (SFA) en Vue.js - CSS, JS y HTML en un solo archivo

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 
 <script setup>
 /*
-1.- Plantilla principal
+Paso 1
 */
+
+const hola = 'Hola en pantalla'
 
 </script>
 
 <template>
-
+<!--
+Paso 2
+ -->
+  <h1> {{ hola }}</h1>
 </template>
 
-<style scoped>
+<style>
 
+/*
+Paso 3
+*/
+h1{
+  text-transform: uppercase;
+  color: red;
+}
 </style>
