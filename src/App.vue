79a0ae361697197d4d441c6627dc9e4564d5d60a
israelonilink <israel_cursos2011@hotmail.com>
2026-03-30 21:34:55 -0600

    43. Conociendo la estructura de Vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,17 @@
 
 <script setup>
-/*
-Paso 1
-*/
 
 const hola = 'Hola en pantalla'
 
 </script>
 
 <template>
-<!--
-Paso 2
- -->
+
   <h1> {{ hola }}</h1>
 </template>
 
 <style>
 
-/*
-Paso 3
-*/
 h1{
   text-transform: uppercase;
   color: red;
