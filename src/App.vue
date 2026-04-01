b736a927d0909bc5dabdfb3d9c3cbc8928872098
israelonilink <israel_cursos2011@hotmail.com>
2026-03-31 21:17:17 -0600

    50. Creando un Componente en Vue.js y pasando datos con Props

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
 
 import {ref, reactive,onMounted} from "vue"
 import {db} from './data/guitarras'
+import Guit from './components/Guitarra.vue'
+import Guitarra from "./components/Guitarra.vue";
 
 const guitarras = ref([])
 /*
@@ -111,20 +113,10 @@ onMounted(() => {
 
     <div class="row mt-5">
 
-      <div v-for="guitarra in guitarras" class="col-md-6 col-lg-4 my-4 row align-items-center">
-        <div class="col-4">
-          <img class="img-fluid" src="/img/guitarra_01.jpg" alt="imagen guitarra">
-        </div>
-        <div class="col-8">
-          <h3 class="text-black fs-4 fw-bold text-uppercase">{{ guitarra.nombre }}</h3>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-          <p class="fw-black text-primary fs-3">$299</p>
-          <button
-              type="button"
-              class="btn btn-dark w-100 "
-          >Agregar al Carrito</button>
-        </div>
-      </div><!-- FIN GUITARRA -->
+      <Guitarra
+          v-for="guitarra in guitarras"
+          :guitarra="guitarra"
+      />
 
     </div>
   </main>
